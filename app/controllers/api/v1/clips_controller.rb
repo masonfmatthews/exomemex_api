9b391d397d6eafd8c223674649864e47d512f404
Mason F. Matthews <mason.f.matthews@gmail.com>
2015-08-14 10:13:40 -0400

    Store clips uploaded at create time.

--- app/controllers/api/v1/clips_controller.rb
+++ app/controllers/api/v1/clips_controller.rb
@@ -2,4 +2,9 @@ class Api::V1::ClipsController < ApplicationController
   def show
     render json: {message: "Made it!"}
   end
+
+  def create
+    clip = Clip.create!(name: params[:name], clip: params[:clip])
+    render json: {clip_url: clip.clip.url}
+  end
 end
