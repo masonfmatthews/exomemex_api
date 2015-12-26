c4c8e13d3bf31d3f4e844a62321efbeb1ff3b178
Mason F. Matthews <mason.f.matthews@gmail.com>
2015-12-25 19:34:19 -0500

    Return id in authentication action.

--- app/controllers/authenticate_controller.rb
+++ app/controllers/authenticate_controller.rb
@@ -2,7 +2,7 @@ class AuthenticateController < ApplicationController
   def login
     u = User.find_by_email(params[:email])
     if u && u.authenticate(params[:password])
-      render json: {token: u.token}
+      render json: {id: u.id, token: u.token}
     else
       render json: "Incorrect email address or password."
     end
